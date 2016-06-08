cbfb2727a4881b881d515dddbe146befcd778cbd
rquast <rquast@rolandquast.com>
2016-06-08 14:53:38 +1000

    Made the json pretty print.

--- src/main/java/com/aof2es/xstream/XStreamUtility.java
+++ src/main/java/com/aof2es/xstream/XStreamUtility.java
@@ -1,15 +1,24 @@
 package com.aof2es.xstream;
 
+import java.io.Writer;
+
 import com.aof2es.xstream.model.ApplicationPreferences;
 import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
 import com.thoughtworks.xstream.io.json.JettisonMappedXmlDriver;
 import com.thoughtworks.xstream.io.json.JsonHierarchicalStreamDriver;
+import com.thoughtworks.xstream.io.json.JsonWriter;
 import com.thoughtworks.xstream.mapper.MapperWrapper;
 
 public final class XStreamUtility {
 
     public static XStream getSerialize() {
-	final XStream xstream = new XStream(new JettisonMappedXmlDriver()) {
+	final XStream xstream = new XStream(new JettisonMappedXmlDriver() {
+	    @Override
+	    public HierarchicalStreamWriter createWriter(final Writer writer) {
+		return new JsonWriter(writer, JsonWriter.DROP_ROOT_MODE);
+	    }
+	}) {
 	    @Override
 	    protected MapperWrapper wrapMapper(MapperWrapper next) {
 		return new MapperWrapper(next) {
@@ -23,6 +32,7 @@ public final class XStreamUtility {
 		    }
 		};
 	    }
+
 	};
 	xstream.setMode(XStream.NO_REFERENCES);
 
